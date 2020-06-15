fc670b4ede59a97dfd7ef2d5133d102c1edc6d7c
Said <said_saidov_1999@mail.ru>
2020-06-15 23:23:37 +0300

    Initial Commit

---
+++ TestNewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestNewsApp
+//
+//  Created by Саид on 15.06.2020.
+//  Copyright © 2020 Саид. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
